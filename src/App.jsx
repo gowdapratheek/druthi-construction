99ab28c2a4859267513fd71075fb09784141b2d1
Pratheek Gowda <gowdapratheek91@gmail.com>
2024-10-12 19:26:11 +0530

    initial commit'

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center m-auto">
+        <Button
+          className="bg-black text-white "
+          variant="outline"
+        >
+          druthi constructions
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
